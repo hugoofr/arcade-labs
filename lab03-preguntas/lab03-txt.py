38b0cdcaa48a2bfcaebccd917339eaff005ca828
Hugo <hugo.flor@edu.uah.es>
2026-02-19 20:26:24 +0100

    JUEGO TERMINADO

--- lab03-preguntas/lab03-txt.py
+++ lab03-preguntas/lab03-txt.py
@@ -29,7 +29,6 @@ lista_banco_preguntas = []
 #Abrimos el archivo y lo procesamos
 with open("lab03-preguntas/preguntas.txt", 'r', encoding = 'utf-8') as archivo:
     for linea in archivo:
-        print(linea.strip())
 
         if linea.strip():
             #Lista de diccionarios que contiene todas las preguntas almacenadas enel banco de preguntas
@@ -37,6 +36,42 @@ with open("lab03-preguntas/preguntas.txt", 'r', encoding = 'utf-8') as archivo:
             #Metemos el diccionario en la lista general
             lista_banco_preguntas.append(diccionario_pregunta)
         
+#ÓGICA DEL JUEGO
 
+puntos_jugador = 0 #Variable que almacenará los puntos del jugador
 
+for pregunta in lista_banco_preguntas:
+    
+    print(pregunta["pregunta"])
 
+    opciones = pregunta["opciones"]
+    print("a) " + opciones[0])
+    print("b) " + opciones[1])
+    print("c) " + opciones[2])
+    print("d) " + opciones[3])
+
+    respuesta_jugador = input("Elige la opción correcta (a, b, c o d):").lower()
+
+    #Traducimos la letra que introdujo el jugador al texto de la respuesta
+    respuesta_elegida = ""
+    if respuesta_jugador == 'a':
+        respuesta_elegida = opciones[0]
+    elif respuesta_jugador == 'b':
+        respuesta_elegida = opciones[1]
+    elif respuesta_jugador == 'c':
+        respuesta_elegida = opciones[2]
+    elif respuesta_jugador == 'd':
+        respuesta_elegida = opciones[3]
+    else:
+        print("Opción no válida. Perdiste tu oportunidad en esta pregunta.")
+
+    #Comprobamos si el jugador ha acertado la pregunta
+    if respuesta_elegida == pregunta["correcta"]:
+        print("¡Correcto! Sumas 5 puntos.\n")
+        puntos_jugador += 5
+    else:
+        print(f"Incorrecto. La respuesta correcta era: {pregunta["correcta"]}\n")
+
+#Fin del juego
+print("¡Terminaste el juego!")
+print("Tu puntuación optenida es = ", puntos_jugador)
