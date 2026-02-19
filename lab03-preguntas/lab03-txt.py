3acb675f7bc0deea40529d37b77d7c48df01fd58
Hugo <hugo.flor@edu.uah.es>
2026-02-19 20:06:12 +0100

    Hecho el paso 3

--- lab03-preguntas/lab03-txt.py
+++ lab03-preguntas/lab03-txt.py
@@ -1,8 +1,3 @@
-#Lógica para visualizar línea a línea el archivo con el banco de preguntas
-with open("lab03-preguntas/preguntas.txt", 'r', encoding = 'utf-8') as archivo:
-    for linea in archivo:
-        print(linea.strip())
-
 #Función que toma una pregunta y sus opciones y devuelve un diccionario estructurado
 def extraer_pregunta(pregunta: str) -> dict:
     
@@ -27,3 +22,21 @@ def extraer_pregunta(pregunta: str) -> dict:
         "correcta": texto_correcta,
         "opciones": lista_opciones
     }
+
+#Lista vacía que será nuestro banco de preguntas final
+lista_banco_preguntas = []
+
+#Abrimos el archivo y lo procesamos
+with open("lab03-preguntas/preguntas.txt", 'r', encoding = 'utf-8') as archivo:
+    for linea in archivo:
+        print(linea.strip())
+
+        if linea.strip():
+            #Lista de diccionarios que contiene todas las preguntas almacenadas enel banco de preguntas
+            diccionario_pregunta = extraer_pregunta(linea)
+            #Metemos el diccionario en la lista general
+            lista_banco_preguntas.append(diccionario_pregunta)
+        
+
+
+
